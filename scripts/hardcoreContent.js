fc3b031d86912b21195239c326d521256953dffd
bofmar <bofmar@outlook.com>
2026-02-08 18:19:57 +0200

    DYanmicaly generate the high end content

---
+++ scripts/hardcoreContent.js
@@ -0,0 +1,296 @@
+content = {
+    arr : {
+        expansionName : "ARR",
+        extremes : [
+            {
+                name : "The Minstrel's Ballad: Ultima's Bane",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "Garuda EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : true}
+                ]
+            },
+            {
+                name : "Titan EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : true}
+                ]
+            },
+            {
+                name : "Ifrit EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : true}
+                ]
+            },
+            {
+                name : "Good King Moggle Mog EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "Leviathan EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "Ramuh EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : true}
+                ]
+            },
+            {
+                name : "Shiva EX",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : true},
+                    {role_name: "dps", cleared : true}
+                ]
+            }
+        ],
+        arr_raids : [
+            {
+                name : "The Binding Coil of Bahamut - Turn 1",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Binding Coil of Bahamut - Turn 2",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Binding Coil of Bahamut - Turn 3",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Binding Coil of Bahamut - Turn 4",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Binding Coil of Bahamut - Turn 5",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 1",
+                roles : [
+                    {role_name : "tank", cleared : true},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 2",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 3",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 4",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Final Coil of Bahamut - Turn 1",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Final Coil of Bahamut - Turn 2",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Final Coil of Bahamut - Turn 3",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Final Coil of Bahamut - Turn 4",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            }
+        ],
+        savage : [
+            {
+                name : "The Second Coil of Bahamut - Turn 1 (Savage)",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 2 (Savage)",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 3 (Savage)",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            },
+            {
+                name : "The Second Coil of Bahamut - Turn 4 (Savage)",
+                roles : [
+                    {role_name : "tank", cleared : false},
+                    {role_name: "healer", cleared : false},
+                    {role_name: "dps", cleared : false}
+                ]
+            }
+        ],
+        ultimate : [],
+        chaotics : [],
+    }
+};
+
+const exImagePath = "../resources/icons/24px-Trial.png";
+const raidImagePath = "../resources/icons/20px-Raid.png";
+const tankImagePath = "../resources/icons/20px-Tank_role.png";
+const healerImagePath = "../resources/icons/20px-Healer_role.png";
+const dpsImagePath = "../resources/icons/20px-DPS_role.png";
+
+const container = document.querySelector(".flex-container");
+
+function createListItem(data, duty_type) {
+    const li = document.createElement("li");
+
+    const duty_icon = document.createElement("img");
+    duty_icon.src = duty_type === "ex" ? exImagePath : raidImagePath;
+    li.appendChild(duty_icon);
+
+    const span = document.createElement("span");
+    span.innerText = data.name;
+    li.appendChild(span);
+
+    data.roles.map(role => {
+        const roleImage = document.createElement("img");
+        roleImage.src = role.role_name === "tank" ? tankImagePath : role.role_name === "healer" ? healerImagePath : dpsImagePath;
+        if (!role.cleared) roleImage.classList.add("uncleared");
+        li.appendChild(roleImage);
+    });
+    return li;
+}
+
+function createList(listName, duties, duty_type) {
+    const title = document.createElement("h2");
+    title.innerText = listName;
+    const list = document.createElement("ul");
+    duties.map( duty => {
+        const item = createListItem(duty, duty_type);
+        list.appendChild(item)
+    })
+
+    return [title, list];
+}
+
+for (const expansion in content) {
+    // create the sectrion
+    const section = document.createElement("section");
+    section.classList.add("clears-section");
+    // create the title
+    const title = document.createElement("h1");
+    title.innerText = content[expansion].expansionName;
+    section.appendChild(title);
+    // create lists
+    if (content[expansion].extremes.length !== 0) {
+        const listElements = createList("Extremes", content[expansion].extremes, "ex");
+        listElements.map( e => {
+            section.appendChild(e);
+        })
+    }
+    if (content[expansion].arr_raids.length !== 0) {
+        const listElements = createList("ARR Raids", content[expansion].arr_raids, "arr_raids");
+        listElements.map( e => {
+            section.appendChild(e);
+        })
+    }
+    if (content[expansion].savage.length !== 0) {
+        const listElements = createList("Savage Tier", content[expansion].savage, "savage");
+        listElements.map( e => {
+            section.appendChild(e);
+        })
+    }
+    if (content[expansion].ultimate.length !== 0) {
+        const listElements = createList("Ultimate", content[expansion].ultimate, "ultimate");
+        listElements.map( e => {
+            section.appendChild(e);
+        })
+    }
+    if (content[expansion].chaotics.length !== 0) {
+        const listElements = createList("Chaotic", content[expansion].chaotics, "chaotic");
+        listElements.map( e => {
+            section.appendChild(e);
+        })
+    }
+
+    container.appendChild(section);
+}
